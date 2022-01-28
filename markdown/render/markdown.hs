952ba3664acdac67455abb1f70b7b86af1e97ba2
Jan-willem De Bleser <jw@thescrapyard.org>
2022-01-28 12:45:20 -0800

    clean up renderer

--- markdown/render/markdown.hs
+++ markdown/render/markdown.hs
@@ -3,6 +3,7 @@ import System.FilePath.Glob
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import Data.Char (isSpace)
+import Data.Functor ((<&>))
 import Data.List
 import CMarkGFM
 
@@ -18,25 +19,27 @@ import CMarkGFM
 -- fix (Node pi ty ls) = Node pi ty $ map fix ls
 
 main = do
-    posts <- glob "../posts/[0-9]*[0-9]"
-    postHtml <- sequence (map postToText $ reverse . sort $ posts) >>= return . joinText
-    indexNodes   <- readToNode "../The Scrapyard"
-    --putStr $ T.unpack $ nToHtml $ addPosts postHtml indexNodes
-    --putStr $ T.unpack $ nToHtml $ addPosts postHtml indexNodes
-    putStr . T.unpack $ nToHtml indexNodes
-    putStr . T.unpack $ makeTag "div" [("class", "h-feed")] $ T.append (makeTag "h3" [("class", "p-name")] $ T.pack "Miscellaneous Ramblings") postHtml
-    where   postToText fn = withFile fn ReadMode parsePostToHtml
-            addPosts posts (Node _ DOCUMENT ls) = Node Nothing DOCUMENT (ls ++ [(Node Nothing (HTML_BLOCK posts) [])])
-            readToNode :: String -> IO Node
-            readToNode fn = TIO.readFile fn >>= return . cmToNode
+    posts <- reverse . sort <$> glob "../posts/[0-9]*[0-9]"
+    postHtml <- T.concat <$> mapM postToText posts
+    readFile "header.html" >>= putStr
+    readToNode "../The Scrapyard" >>= TIO.putStr . nToHtml
+    TIO.putStr $ makeTag "div" [("class", "h-feed")] $ T.append ramblings postHtml
+    readFile "footer.html" >>= putStr
+    where  
+        ramblings = makeTag "h3" [("class", "p-name")] $ T.pack "Miscellaneous Ramblings"
+        postToText :: FilePath -> IO T.Text
+        postToText fn = withFile fn ReadMode parsePostToHtml
+        addPosts posts (Node _ DOCUMENT ls) = Node Nothing DOCUMENT (ls ++ [Node Nothing (HTML_BLOCK posts) []])
+        readToNode :: String -> IO Node
+        readToNode fn = cmToNode <$> TIO.readFile fn
 
 parsePostToHtml :: Handle -> IO T.Text
 parsePostToHtml h = do
     title  <- TIO.hGetLine h
     date   <- TIO.hGetLine h
-    author <- TIO.hGetLine h >>= return . head . nodeChildren . head . nodeChildren . cmToNode --Author link produces a PARAGRAPH in a DOCUMENT, and in there the LINK
+    author <- TIO.hGetLine h <&> head . nodeChildren . head . nodeChildren . cmToNode --Author link produces a PARAGRAPH in a DOCUMENT, and in there the LINK
     anchor <-     hGetLine h
-    body   <- TIO.hGetContents h >>= return . cmToHtml
+    body   <- cmToHtml <$> TIO.hGetContents h
 
     return $ makeTag "article" [("class", "h-entry")] $ joinText [makeTitleHtml anchor title, makeBylineHtml author date anchor, inDiv "e-content" body]
 
@@ -56,7 +59,7 @@ parsePostToHtml h = do
 
 
 makeTag :: String -> [(String, String)] -> T.Text -> T.Text
-makeTag cls attrs body = joinText $ (map T.pack $ ["<", cls] ++ (map attrToHtml attrs) ++ [">"]) ++ [body] ++ (map T.pack ["</", cls, ">\n"])
+makeTag cls attrs body = joinText $ map T.pack (["<", cls] ++ map attrToHtml attrs ++ [">"]) ++ [body] ++ map T.pack ["</", cls, ">\n"]
     where   attrToHtml :: (String, String) -> String
             attrToHtml (key, val) = ' ' : key ++ "=\"" ++ val ++ "\""
 
